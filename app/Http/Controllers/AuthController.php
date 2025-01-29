17958e53e82f58905e46e4d3956519e8f256f55d
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-29 12:46:09 +0330

    create login process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreLoginRequest;
 use App\Http\Requests\StoreRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -13,7 +14,12 @@ class AuthController extends Controller
         return view('auth.login');
     }
     public function login(StoreLoginRequest $request){
-
+        $myData = $request->only('email', 'password');
+        if(Auth::attempt($myData)){
+            return to_route('dashboard');
+        }else{
+            return redirect()->back();
+        }
     }
     public function registerForm(){
         return view('auth.register');
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/',[UserController::class,'dashboard'])->name('dashboard');
 
 Route::get('/login',[AuthController::class,'loginForm'])->name('login.form');
