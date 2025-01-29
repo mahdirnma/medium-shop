f483ad2a66fc0ac6c574302e7198b2fe3835dbde
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-29 12:24:11 +0330

    migrate

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::create([
+            'title' => 's25',
+            'description' => 'lorem ipsum',
+            'price'=>'50000000'
+        ]);
+        Product::create([
+            'title' => 's25 +',
+            'description' => 'lorem ipsum 2',
+            'price'=>'65000000'
+        ]);
+
     }
 }
